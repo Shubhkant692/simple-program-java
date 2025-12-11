eb577b883ae6b50b2442ebcec52e333756929d3a
Shubhkant692 <shubhkantchaudhary0@gmail.com>
2025-12-11 20:57:58 +0545

    Create if statement program.java

---
+++ if statement program.java
@@ -0,0 +1,12 @@
+class Test20{
+    public static void main(String a[])
+    {
+        int x=30;
+        if(x<30);
+        {
+            System.out.println("this is if statement");
+        }
+        System.out.println("Bye");
+        System.out.println("We are BEIT students");
+    }
+}
